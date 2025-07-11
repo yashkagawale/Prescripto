9b0d560ebe3e0ba3cc646a389a6916efb76423da
Yash Kagawake <kagawaleyash@gmail.com>
2025-07-11 16:28:41 +0530

    Header & Speciality Commit
    Added the header and the find by speciality section on the home page.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,20 @@
+/**
+ * Node modules
+ */
 import React from 'react'
+
+/**
+ * Components
+ */
+import Header from '../components/Header'
+import SpecialityMenu from '../components/SpecialityMenu'
+
 
 const Home = () => {
   return (
     <div>
-        
+        <Header />
+        <SpecialityMenu />
     </div>
   )
 }
