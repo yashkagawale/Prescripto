94b7919adf2caa56e9519ad2dbc4ded407225207
Yash Kagawake <kagawaleyash@gmail.com>
2025-07-19 22:47:50 +0530

    Update vite.config.js
    Defined the port number for the frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(),
-    tailwindcss(),
+  tailwindcss(),
   ],
-  server:{port:5173}
+  server: { port: 5173 }
 })
